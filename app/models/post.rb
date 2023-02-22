cac680f5c6d5b460daa4be6a1d404cfb7ef414ec
Liza McLain <lizamclain@gmail.com>
2023-02-22 12:09:11 -0600

    commit tree 01b7061c6f909db4c3340096da002b983f306e61

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, :length => {minimum: 250}
+    validates :summary, :length => {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+
+    validate :validate_clickbaity
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def validate_clickbaity
+        # if title.includes? `Won't Believe` || 'Secret' || 'Top [number]' || 'Guess'
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
